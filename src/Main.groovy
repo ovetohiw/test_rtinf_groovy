9fbebffe76d5112c4aa5b73ad7d125734cce2d0e
red_b <@ovetohiw>
2025-02-09 05:35:37 +0500

    feat: rtinf_groovy

---
+++ src/Main.groovy
@@ -0,0 +1,17 @@
+class Main {
+    static void main(String[] args) {
+
+        def arr = [1, 3, 4, 5, 1, 5, 4]
+
+        System.out.println(associativeArray(arr))
+    }
+
+    def static associativeArray(def arr) {
+        def map = [:]
+        for (i in arr) {
+            map[i] = map.get(i, 0) + 1
+        }
+
+        map
+    }
+}
